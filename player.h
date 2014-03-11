d9111592777c0ead86b9979cad6837f5b613a79c
student <student@cs2-VirtualBox.(none)>
2014-03-10 23:45:39 -0700

    Changed heuristic

--- player.h
+++ player.h
@@ -27,10 +27,10 @@ private:
     Move* randomMove();
     Move* simpleHeuristicMove();
     std::list<Move*>* possibleMoves(Board* tempBoard,Side side);
-    void heuristic(Move* move,Side side,Board* originalBoard);
+    int heuristic(Move* move,Side side,Board* originalBoard);
     Move* miniMaxMove(int depth);
-    DecisionTreeNode* findMin(std::list<DecisionTreeNode*>* list);
-    DecisionTreeNode* findMax(std::list<DecisionTreeNode*>* list);
+    DecisionTreeNode* findMax (std::list<DecisionTreeNode*>* list);
+    DecisionTreeNode* findMin (std::list<DecisionTreeNode*>* list);
 };
 
 #endif
